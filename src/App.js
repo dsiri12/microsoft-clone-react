4eecabb5eee1920c67fc0f5ee451a9186d751831
Damien Siri <dsiri12@gmail.com>
2021-06-13 12:56:24 -0400

    done navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Banner from './components/Banner'
 
 const App = () => {
   return (
-    <div>
-      <h1>hi damien</h1>
+    <div className="container">
+      {/* Nav */}
+      <Navbar />
+
+      <Banner />
     </div>
   )
 }
 
 export default App
+
+// https://www.youtube.com/watch?v=uKgn-To1C4Q&list=LL&index=1&t=91s
+// https://codepen.io/bradtraversy/details/ZEGGNRb
---
+++ src/components/Banner.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Banner = () => {
+  return <header className="showcase">Banner123</header>
+}
+
+export default Banner
