75ce68c9b1aa0ab5546d2e7ec770e3b7d5010812
Damien Siri <dsiri12@gmail.com>
2021-06-15 12:57:43 -0400

    saveOne-microsoftClone

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Banner from './components/Banner'
+import HomeCards1 from './components/HomeCards1'
+import Xbox from './components/Xbox'
+import HomeCards2 from './components/HomeCards2'
+import Carbon from './components/Carbon'
+import Follow from './components/Follow'
+import Links from './components/Links'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -9,6 +16,20 @@ const App = () => {
       <Navbar />
 
       <Banner />
+
+      <HomeCards1 />
+
+      <Xbox />
+
+      <HomeCards2 />
+
+      <Carbon />
+
+      <Follow />
+
+      <Links />
+
+      <Footer />
     </div>
   )
 }
